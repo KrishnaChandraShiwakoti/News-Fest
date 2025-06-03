0de73e4d34b613b4f44cd27421ee92317054b10d
Rijan Shiwakoti <rijankrishna14@gmail.com>
2025-06-03 13:26:16 +0545

    added admin side of the web

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,5 +1,29 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Login } from "./Pages";
+
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Landing />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
