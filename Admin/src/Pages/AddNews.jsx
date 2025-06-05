0a0b5deea7529b5e77af86a4ee95c6caf3255e53
Rijan Shiwakoti <rijankrishna14@gmail.com>
2025-06-05 21:31:27 +0545

    create  news page added

--- Admin/src/Pages/AddNews.jsx
+++ Admin/src/Pages/AddNews.jsx
@@ -1,19 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import "../Styles/AddNews.css";
 const AddNews = () => {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(URL.createObjectURL(file));
+    }
+  };
   return (
-    <div>
-      <div className="title-container">
-        <h1>News</h1>
-        <div>
+    <form>
+      <div className="title">
+        <h1>Add new News</h1>
+        <div className="flex">
           <Link to={"/news"} className="cancel">
             Cancel
           </Link>
-          <Link to={"/news/add"}>New Article</Link>
+          <Link to={"/news/add"} type="submit" className="publish">
+            Publish
+          </Link>
         </div>
       </div>
-    </div>
+      <div className="section-container">
+        <div className="input-section">
+          <input type="text" placeholder="News Title" name="title" required />
+          <textarea
+            name="content"
+            id=""
+            placeholder="Write your news content here"
+            rows="10"
+            cols="10"
+            required></textarea>
+        </div>
+        <div className="details-section">
+          <h3>News Details</h3>
+          <hr />
+          <label htmlFor="status" status>
+            Status
+          </label>
+          <select name="status" id="">
+            <option value="draft">Draft</option>
+            <option value="published">published</option>
+          </select>
+          <label htmlFor="category">Category</label>
+          <select name="category" id="">
+            <option value="world">World</option>
+            <option value="news">News</option>
+          </select>
+        </div>
+      </div>
+      <div className="featured-image-container">
+        <h3>Featured Image</h3>
+        <div className="image-preview">
+          {image ? (
+            <img src={image} alt="Selected" />
+          ) : (
+            <div className="placeholder">
+              <span role="img" aria-label="image icon">
+                🖼️
+              </span>
+              <p>No image selected</p>
+            </div>
+          )}
+        </div>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          id="fileUpload"
+          className="file-input"
+        />
+      </div>
+    </form>
   );
 };
 
